b3e496b84afae60f265d7bca53bd075b4eda27f9
nguyenduckien0905 <kenzo992005@gmail.com>
2026-09-14 20:07:28 +0700

    lab1

--- learn-typescript/src/lab1.ts
+++ learn-typescript/src/lab1.ts
@@ -21,4 +21,29 @@ function tinhTong(...numbers: number[]): number {
 console.log(tinhTong(1, 2, 3));
 
 //Viết hàm trả về số lượng xuất hiện của 1 kí tự trong chuỗi
-
+function demKyTu(chuoi: string, kyTu: string): number {
+    let dem = 0;
+    for (let i = 0; i < chuoi.length; i++) {
+        if (chuoi[i] === kyTu) {
+            dem++;
+        }
+    }
+    return dem;
+}
+console.log(demKyTu("hello", "l"));
+console.log(demKyTu("nguyenduckien", "k"));
+// so nguyen to
+function laSoNguyenTo(n: number): boolean {
+    if (n < 2) {
+        return false;
+    }
+    for (let i = 2; i < n; i++) {
+        if (n % i === 0) {
+            return false;
+        }
+    }
+    return true;
+}
+console.log(laSoNguyenTo(7));
+console.log(laSoNguyenTo(10));
+console.log(laSoNguyenTo(1)); 
