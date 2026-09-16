5cff0e539fe01e468717066812c2d5209f87031f
nguyenduckien0905 <kenzo992005@gmail.com>
2026-09-16 19:03:06 +0700

    string number boolean

--- learn-typescript/src/typescript.ts
+++ learn-typescript/src/typescript.ts
@@ -1,1 +1,16 @@
 console.log("typescript");
+
+let age:number=25;
+age=30;
+
+let myName:string="kiendubai";
+myName="20";
+
+ let dihockhong:boolean = true;
+ dihockhong= false;
+
+ const student :{name: string ; age: number ; isActive: boolean}={
+    name:"kien",
+    age:21,
+    isActive:true,
+ };
