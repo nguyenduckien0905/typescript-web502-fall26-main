9098991eb9cc4cbbc436b877d88ee8448d3db582
nguyenduckien0905 <kenzo992005@gmail.com>
2026-09-16 19:27:51 +0700

    lab2

---
+++ learn-typescript/src/lab2.ts
@@ -0,0 +1,21 @@
+enum Rate {
+    low = 'Thấp',
+    medium = 'Trung bình',
+    hight = 'Cao'
+}
+
+type Product = {
+    name: string;
+    price: number;
+    sale: boolean;
+    rate: Rate;
+};
+
+let listProducts: Product[] = [
+    { name: 'Áo thun', price: 10,sale: true, rate: Rate.hight},
+    { name: 'Quần jean', price: 8, sale: false, rate: Rate.medium},
+    { name: 'Giày thể thao', price: 15, sale: true, rate: Rate.hight},
+    { name: 'Mũ', price: 5, sale: true, rate: Rate.low},
+    { name: 'Túi xách', price: 3, sale: false, rate: Rate.medium}
+];
+
