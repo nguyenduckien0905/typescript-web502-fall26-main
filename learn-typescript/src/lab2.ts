2e534c75bcc9ead6a304d75ee415030738e3d796
nguyenduckien0905 <kenzo992005@gmail.com>
2026-09-18 19:26:20 +0700

    lab2

--- learn-typescript/src/lab2.ts
+++ learn-typescript/src/lab2.ts
@@ -19,3 +19,42 @@ let listProducts: Product[] = [
     { name: 'Túi xách', price: 3, sale: false, rate: Rate.medium}
 ];
 console.log(listProducts);
+//
+function addDescription() {
+  listProducts = listProducts.map(product => ({
+    ...product,
+    description: product.price > 5 ? "Tốt" : "Bình thường"
+  }));
+  console.log(listProducts);
+}
+// hien thi san pham bang foreach
+function showProducts(){
+    listProducts.forEach(product=>{
+        console.log(
+            "ten san pham :",product.name,
+            "gia ban :",product.price,
+            "sale:",product.sale,
+            "danh gia:",product.rate,
+        );
+    });
+}
+//tinh tong bang reduce
+function tonggiaban(){
+    let tong = listProducts.reduce((sum,product)=>{
+        return sum + product.price;
+    },0);
+    console.log("tong gia ban:",tong);
+}
+ //loc san pham sale danh gia trung binh
+ function filterProducts(){
+    let result=listProducts.filter(product=>{
+        product.sale===true&&( product.rate ===Rate.medium ||product.rate===Rate.hight)
+    });
+    console.log(result);
+    
+ }   
+ addDescription();
+showProducts();
+tonggiaban();
+filterProducts();
+
--- learn-typescript/src/typescript.ts
+++ learn-typescript/src/typescript.ts
@@ -14,3 +14,22 @@ myName="20";
     age:21,
     isActive:true,
  };
+
+ // enum
+ enum status{
+   done ="hoan thanh ",
+   doing="dang lam"
+ }
+ //array
+const numbers: number[] = [1, 2, 3, 4];
+const students: string[] = ["nam", "an"];
+const products: { name: string }[] = [{ name: "laptop" }, { name: "laptop" }];
+//any
+let data :any ="data";
+data =1;
+//union:kethop
+let result:string| number="ket qua";
+result=3;
+//literal
+let doSomething: "success" |"error" ="success";
+let value :null|undefined;
